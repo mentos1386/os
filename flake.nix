5ce36fd648ce8c58170efa5af48088d90106c20e
Tine <tine@tjo.space>
2026-08-16 10:46:10 +0200

    chore: switch to nix develop from devbox

---
+++ flake.nix
@@ -0,0 +1,30 @@
+{
+  description = "mentos1386/os";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      rec {
+        devShells.default = pkgs.mkShellNoCC {
+          packages = with pkgs; [
+            buildah
+            skopeo
+            cosign
+          ];
+        };
+      }
+    );
+}
